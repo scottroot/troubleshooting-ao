22d3b0ba33261f0686f438a469a43ce2e1011507
Scott <55364794+scottroot@users.noreply.github.com>
2024-05-18 10:31:21 -0400

    Update loader.test.js

--- test/loader.test.js
+++ test/loader.test.js
@@ -14,6 +14,20 @@ const env = {
   }
 }
 
+const msg = {
+  Target: 'AOS',
+  Owner: 'Test',
+  ['Block-Height']: "1000",
+  Id: "1234xyxfoo",
+  Module: "WOOPAWOOPA",
+  Tags: [{ name: 'Action', value: 'Eval' }],
+  Data: `
+local scott = require("scott")
+
+return scott.hello()
+      `
+}
+
 describe('Basic ao-loader tests.', function () {
   it('Test Load process.wasm: wasm64-unknown-emscripten-draft_2024_02_15', async () => {
     const options = { format: "wasm64-unknown-emscripten-draft_2024_02_15", computeLimit: 9e12 }
@@ -22,27 +36,57 @@ describe('Basic ao-loader tests.', function () {
     console.log("WASM module format:", options.format)
     console.log("WASM module compute limit:", options.computeLimit)
     let total_gas = 0
-
     const handle = await AoLoader(wasm, options)
-
-    let result = await handle(null, {
-      Target: 'AOS',
-      Owner: 'Test',
-      ['Block-Height']: "1000",
-      Id: "1234xyxfoo",
-      Module: "WOOPAWOOPA",
-      Tags: [{ name: 'Action', value: 'Eval' }],
-      Data: `
-local scott = require("scott")
-
-return scott.hello()
-          `
-    },
-      env
-    )
-
+    let result = await handle(null, msg, env)
     total_gas += result.GasUsed
+  })
 
+  it('Test Load process.wasm: wasm32-unknown-emscripten-draft_2024_02_15', async () => {
+    const options = { format: "wasm32-unknown-emscripten-draft_2024_02_15", computeLimit: 9e12 }
+    const wasm = fs.readFileSync('process.wasm')
+    console.log("WASM image loaded. Size:", (wasm.length / 1024 / 1024).toFixed(2), " MB")
+    console.log("WASM module format:", options.format)
+    console.log("WASM module compute limit:", options.computeLimit)
+    let total_gas = 0
+    const handle = await AoLoader(wasm, options)
+    let result = await handle(null, msg, env)
+    total_gas += result.GasUsed
+  })
+
+  it('Test Load process.wasm: wasm32-unknown-emscripten', async () => {
+    const options = { format: "wasm32-unknown-emscripten", computeLimit: 9e12 }
+    const wasm = fs.readFileSync('process.wasm')
+    console.log("WASM image loaded. Size:", (wasm.length / 1024 / 1024).toFixed(2), " MB")
+    console.log("WASM module format:", options.format)
+    console.log("WASM module compute limit:", options.computeLimit)
+    let total_gas = 0
+    const handle = await AoLoader(wasm, options)
+    let result = await handle(null, msg, env)
+    total_gas += result.GasUsed
+  })
+  
+  it('Test Load process.wasm: wasm32-unknown-emscripten2', async () => {
+    const options = { format: "wasm32-unknown-emscripten2", computeLimit: 9e12 }
+    const wasm = fs.readFileSync('process.wasm')
+    console.log("WASM image loaded. Size:", (wasm.length / 1024 / 1024).toFixed(2), " MB")
+    console.log("WASM module format:", options.format)
+    console.log("WASM module compute limit:", options.computeLimit)
+    let total_gas = 0
+    const handle = await AoLoader(wasm, options)
+    let result = await handle(null, msg, env)
+    total_gas += result.GasUsed
+  })
+  
+  it('Test Load process.wasm: wasm32-unknown-emscripten3', async () => {
+    const options = { format: "wasm32-unknown-emscripten3", computeLimit: 9e12 }
+    const wasm = fs.readFileSync('process.wasm')
+    console.log("WASM image loaded. Size:", (wasm.length / 1024 / 1024).toFixed(2), " MB")
+    console.log("WASM module format:", options.format)
+    console.log("WASM module compute limit:", options.computeLimit)
+    let total_gas = 0
+    const handle = await AoLoader(wasm, options)
+    let result = await handle(null, msg, env)
+    total_gas += result.GasUsed
   })
 
 })
